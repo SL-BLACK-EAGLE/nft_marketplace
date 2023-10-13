5ca9b7b1409b6f0eaa41b323330ab2df4a12298f
SL-BLACK-EAGLE <chathudreamshms@gmail.com>
2023-10-13 21:18:40 +0530

    make navbar

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,9 +1,7 @@
-import ThemeProviderWrapper from "@/app/theme-provider";
 import React from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Script from "next/script";
-import {ThemeProvider} from "next-themes";
 
 export default function MainLayout({
                                        children,
@@ -12,13 +10,12 @@ export default function MainLayout({
 }) {
     return (
         <main className="dark:bg-nft-dark bg-white min-h-screen">
-                    <Navbar/>
-                    <div className="pt-65">
-                        {children}
-                    </div>
-                    <Footer />
-
-                <Script src="https://kit.fontawesome.com/d45b25ceeb.js" crossorigin="anonymous" />
+            <Navbar/>
+            <div className="pt-65">
+                {children}
+            </div>
+            <Footer />
+            <Script src="https://kit.fontawesome.com/d45b25ceeb.js" crossorigin="anonymous" />
         </main>
 
 
