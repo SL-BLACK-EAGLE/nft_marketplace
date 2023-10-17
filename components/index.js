d7e017d3452202a497f86e4dffbc4920afed0453
SL-BLACK-EAGLE <chathudreamshms@gmail.com>
2023-10-17 16:37:25 +0530

    Top sellers part complete

--- components/index.js
+++ components/index.js
@@ -2,5 +2,6 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import Banner from './Banner';
 import Button from './ui/Button';
+import CreatorCard from './CreatorCard';
 
-export { Navbar, Footer, Button, Banner };
+export { Navbar, Footer, Button, Banner, CreatorCard };
